482a2a76d3c3cd66ebb0776889d5cb62f6617843
Andreoid25 <Andremndnts@gmail.com>
2024-01-12 21:17:30 +0800

    undo comment

--- gallery.php
+++ gallery.php
@@ -458,7 +458,6 @@ $uploadedImages = $stmtFetchImages->fetchAll(PDO::FETCH_ASSOC);
                 integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
 <script>
-//WASASWAASA
 
 
 $(document).ready(function () {
